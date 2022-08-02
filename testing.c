b91d88a85913995a253cdce4d1dcd5edada7a6a0
Michiel Teerling <mteerlin@f1r4s17.codam.nl>
2022-08-02 12:33:20 +0200

    Hello Branch

--- testing.c
+++ testing.c
@@ -6,14 +6,25 @@
 /*   By: mteerlin <mteerlin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/02 12:12:03 by mteerlin      #+#    #+#                 */
-/*   Updated: 2022/08/02 12:18:55 by mteerlin      ########   odam.nl         */
+/*   Updated: 2022/08/02 12:31:58 by mteerlin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <unistd.h>
+
+int	b_printf(char *str)
+{
+	int	cnt;
+
+	cnt = 0;
+	while (str[cnt] != '\0')
+		cnt++;
+	write(1, str, cnt);
+	return (cnt);
+}
 
 int	main(void)
 {
-	printf("Hello World!\n");
+	b_printf("Hello World!\n");
 	return (0);
 }
